9d523ab2795f84c917696ecddc9ba7d9c9b43e1e
Vanessa <vanessa-almeida@prodesan.com.br>
2022-04-12 09:24:12 -0300

    Component de Curso e pagina Home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="home">Listagem dos Cursos</div>
+  <div>
+    <div class="pageTitle">
+      <span class="title">Meus Cursos</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div class="content">
+      <ul class="listCourses">
+        <card-course
+          :title="'Curso Laravel com Vue.js'"
+          :image="''"
+          :bgName="'laravel'"
+        >
+        </card-course>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import CardCourse from "./components/CardCourse.vue";
+
 export default {
   name: "Home",
+  components: {
+    CardCourse,
+  },
 };
 </script>
