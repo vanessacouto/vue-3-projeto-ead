fe9cbc8f35d69962c7ea03963d44692dd0d13cb3
Vanessa <vanessa-almeida@prodesan.com.br>
2022-04-13 08:32:03 -0300

    Página de Autenticação e Esquecimento de senha

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/Home.vue'
 import MySupports from '@/views/supports/MySupports.vue'
+import ModulesAndLessons from '@/views/modules/ModulesAndLessons.vue'
+import Auth from '@/views/auth/Auth.vue'
+import ForgetPassword from '@/views/auth/ForgetPassword.vue'
 
 const routes = [
   {
     path: '/campus',
     component: () => import('@/layouts/DefaultTemplate.vue'),
     children: [
+      {
+        path: 'modules',
+        name: 'campus.modules',
+        component: ModulesAndLessons
+      },
       {
         path: 'minhas-duvidas',
         name: 'campus.my.supports',
@@ -18,7 +26,17 @@ const routes = [
         component: Home
       }
     ]
-  }
+  },
+  {
+    path: '/',
+    name: 'auth',
+    component: Auth
+  },
+  {
+    path: '/recuperar-senha',
+    name: 'forget.password',
+    component: ForgetPassword
+  },
 ]
 
 const router = createRouter({
