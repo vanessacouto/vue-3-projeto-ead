595397becb4942b9d645eee981dcda0420b5c442
Vanessa <vanessa-almeida@prodesan.com.br>
2022-04-26 10:51:32 -0300

    Carregar curso selecionado para o vuex

--- src/views/home/components/CardCourse.vue
+++ src/views/home/components/CardCourse.vue
@@ -2,14 +2,14 @@
   <li>
     <span :class="['card', 'bg-' + bgName]">
       <span class="icon">
-        <img :src="[image != '' ? image : require('@/assets/images/icons/default2.png')]" :alt="title" />
+        <img :src="[course.image != '' ? course.image : require('@/assets/images/icons/default2.png')]" :alt="course.name" />
       </span>
-      <span class="title">{{ title }}</span>
+      <span class="title">{{ course.name }}</span>
       <span class="action">
-        <!-- <a href="trainings.html" :class="['btn', bgName]">Acessar</a> -->
-        <router-link :to="{name: 'campus.modules'}" :class="['btn', bgName]">
+        <!-- <router-link :to="{name: 'campus.modules'}" :class="['btn', bgName]">
           Acessar
-        </router-link>
+        </router-link> -->
+        <a href="#" @click.prevent="setCourse" :class="['btn', bgName]">Acessar</a>
       </span>
     </span>
     <span class="dots">
@@ -21,8 +21,27 @@
 </template>
 
 <script>
+import { useStore } from 'vuex'
+
+import router from '@/router'
+
 export default {
   name: "CardCourse",
-  props: ['title', 'image', 'bgName']
+  props: ['course', 'bgName'],
+  setup(props) {
+    const store = useStore()
+
+    const setCourse = () => {
+      // chama o mutation direto
+      store.commit('SET_COURSE_SELECTED', props.course) // passa o course que passamos via o component 'card-course'
+
+      // redireciona
+      router.push({name: 'campus.modules'})
+    }
+
+    return {
+      setCourse
+    }
+  }
 };
 </script>
