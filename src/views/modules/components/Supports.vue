fc51f0365075a01a3a78b8a3096a40ceb5d6d509
Vanessa <vanessa-almeida@prodesan.com.br>
2022-04-27 11:46:15 -0300

    Carregando dúvidas da API

--- src/views/modules/components/Supports.vue
+++ src/views/modules/components/Supports.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="comments" v-show="lesson.name">
     <div class="header">
-      <span class="title">Dúvidas</span>
+      <span class="title">Dúvidas (total: {{ supports.length }}) <span v-if="loading">(Carregando...)</span> </span>
       <button class="btn primary">
         <i class="fas fa-plus"></i>
         Enviar nova dúvida
@@ -14,7 +14,7 @@
 
 <script>
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 
 import Supports from "@/components/Supports";
 
@@ -24,11 +24,27 @@ export default {
     const store = useStore();
 
     // aula que está sendo exibida no player
-    const lesson = computed(() => store.state.courses.lessonPlayer)
+    const lesson = computed(() => store.state.courses.lessonPlayer);
+    const supports = computed(() => store.state.supports.supports.data);
+    const loading = ref(false);
+
+    // só vai chamar a action se o valor de 'lesson' mudar
+    watch(
+      () => store.state.courses.lessonPlayer, // observa o valor de lessonPlayer
+      () => {
+        // ação que deve executar se o valor de lessonPlayer mudar
+        loading.value = true;
+        store
+          .dispatch("getSupportsOfLesson", lesson.value.id) // pega as duvidas da aula que mudou
+          .finally(() => (loading.value = false)); // quando finalizar, retorna o loading para false
+      }
+    );
 
     return {
-      lesson
-    }
+      lesson,
+      loading,
+      supports
+    };
   },
   components: {
     Supports,
