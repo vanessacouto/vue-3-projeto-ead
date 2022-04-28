97edce50fd5c87aaf4eaa1c824b9eb64490dee5d
Vanessa <vanessa-almeida@prodesan.com.br>
2022-04-28 14:24:41 -0300

    Ocultar ou exibir respostas

--- src/components/Supports.vue
+++ src/components/Supports.vue
@@ -5,9 +5,9 @@
         <span class="avatar">
           <img
             :src="[
-              support.user.image ?
-              support.user.image :
-              require('@/assets/images/avatars/user01.svg')
+              support.user.image
+                ? support.user.image
+                : require('@/assets/images/avatars/user01.svg'),
             ]"
             :alt="support.user.name"
           />
@@ -21,14 +21,36 @@
             <span class="text">{{ support.description }}</span>
           </div>
         </div>
-        <button class="btn primary">Ocultar respostas</button>
+        <button class="btn primary">
+          <span
+            v-if="showSupport === support.id"
+            @click.prevent="showSupport = '0'"
+            >Ocultar respostas</span
+          >
+          <span v-else @click.prevent="showSupport = support.id"
+            >Exibir respostas (total: {{ support.replies.length }})</span
+          >
+        </button>
       </div>
-      <div
-        class="answersContent"
-        v-for="reply in support.replies"
-        :key="reply.id"
-      >
-        <div class="commentContent rightContent">
+      <div class="answersContent" v-show="showSupport === support.id">
+        <div
+          v-for="reply in support.replies"
+          :key="reply.id"
+          :class="[
+            'commentContent',
+            { rightContent: support.user.id != reply.user.id },
+          ]"
+        >
+          <span class="avatar" v-if="support.user.id == reply.user.id">
+            <img
+              :src="[
+                reply.user.image
+                  ? reply.user.image
+                  : require('@/assets/images/avatars/user01.svg'),
+              ]"
+              :alt="reply.user.name"
+            />
+          </span>
           <div class="comment">
             <div class="balloon">
               <span class="fas fa-sort-down"></span>
@@ -38,31 +60,16 @@
               <span class="text" v-text="reply.description"></span>
             </div>
           </div>
-          <span class="avatar">
+          <span class="avatar" v-if="support.user.id != reply.user.id">
             <img
               :src="[
-          reply.user.image ?
-          reply.user.image :
-          require('@/assets/images/avatars/user01.svg')
-          ]"
+                reply.user.image
+                  ? reply.user.image
+                  : require('@/assets/images/avatars/user01.svg'),
+              ]"
               :alt="reply.user.name"
             />
           </span>
-        </div>
-        <div class="commentContent">
-          <span class="avatar">
-            <img src="images/avatars/user01.svg" alt="" />
-          </span>
-          <div class="comment">
-            <div class="balloon">
-              <span class="fas fa-sort-down"></span>
-              <span class="owner">Fernando - 07/10/2021</span>
-              <span class="text">
-                In eleifend urna sapien, faucibus pharetra justo luctus quis.
-                Vivamus eleifend fringilla massa
-              </span>
-            </div>
-          </div>
         </div>
         <span class="answer">
           <button class="btn primary">Responder</button>
@@ -73,18 +80,21 @@
 </template>
 
 <script>
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
-import { computed } from "vue";
 
 export default {
   name: "Supports",
   setup() {
     const store = useStore();
 
-    const supports = computed(() => store.state.supports.supports);
+    const showSupport = ref("0");
 
+    const supports = computed(() => store.state.supports.supports);
+    
     return {
       supports,
+      showSupport,
     };
   },
 };
