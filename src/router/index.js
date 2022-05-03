55f5202f9d74b720991560badd72e358b698db30
Vanessa <vanessa-almeida@prodesan.com.br>
2022-05-03 11:13:21 -0300

    Aplicando filtros em rotas

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import Auth from '@/views/auth/Auth.vue'
 import ForgetPassword from '@/views/auth/ForgetPassword.vue'
 import ResetPassword from '@/views/auth/ResetPassword.vue'
 
+import store from '@/store'
+import { TOKEN_NAME } from '@/configs'
+
 const routes = [
   {
     path: '/campus',
@@ -49,6 +52,27 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// para cada url que é chamada, vai executar esses passos
+router.beforeEach(async (to, _, next) => {
+  const loggedIn = store.state.users.loggedIn
+
+  if (to.name != 'reset.password' && !loggedIn) { // se não estiver na pagina de reset password e não estiver autenticado
+    const token = await localStorage.getItem(TOKEN_NAME) // recupera o token
+    
+    if (!token && to.name != 'auth' && to.name != 'forget.password') {
+      return router.push({ name: 'auth' }) // redireciona pra pagina de autenticação
+    }
+
+    // se tem o toke e não está em nenhuma das 3 paginas (reset.password, auth ou forget.password)
+    await store.dispatch('getMe') // vai chamar o getMe
+      .catch(() => { // se der algum erro
+        if (to.name != 'auth') return router.push({ name: 'auth' }) // redireciona pra pagina de auth
+      })
+  }
+
+  next() // dando tudo ok, prossegue normalmente
 })
 
 export default router
