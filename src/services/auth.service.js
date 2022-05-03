7e3f3411c6f5bd6680b6f9661be65e386195849e
Vanessa <vanessa-almeida@prodesan.com.br>
2022-05-03 10:51:07 -0300

    Recuperar usuário autenticado

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -14,4 +14,23 @@ export default class AuthService extends BaseService {
                 .catch(error => reject(error.response))
         })
     }
+
+
+    static async getMe() {
+        // pode ser que o token ainda nem exista, então evitamos uma requisição para a API
+        const token = await localStorage.getItem(TOKEN_NAME)
+
+        if(!token) {
+            return Promise.reject('Token Not Found')
+        }
+
+        return new Promise((resolve, reject) => {
+            this.request({auth: true})
+                .get('/me')
+                .catch(error => { // quando cair no erro, vai deletar o token
+                    localStorage.removeItem(TOKEN_NAME)
+                    reject(error.response)
+                }) 
+        })
+    }
 }
